23bc1364937708f7fafc2d273f4b55603a49f7c3
Mariya Kochanova <balalum@gmail.com>
2026-04-24 13:14:26 +0200

    header + basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Header from './Components/Header/Header'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
+      <Header />
       <h1>Anna</h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './locales/en/translation.json';
+import ru from './locales/ru/translation.json';
+import es from './locales/es/translation.json';
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: { translation: en },
+            ru: { translation: ru },
+            es: { translation: es },
+        },
+        lng: 'en',
+        fallbackLng: 'en',
+
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+
+export default i18n;
